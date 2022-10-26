1332bbdbbae300f94263c4ec4cee84734bdba8bb
Jakaria230161 <jakariamahmud016@gmail.com>
2022-10-26 23:30:46 +0600

    blog setup

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,74 +2,175 @@ import React from "react";
 
 export default function Blog() {
   return (
-    <div className="p-16 ">
-      <h1 className="text-5xl text-center mb-10">Blog</h1>
-      <div className="mb-5 p-10 text-start border border-base-400 bg-base-200 rounded-box">
-        <div className=" text-xl font-medium">1. What is cors?</div>
-        <div className="">
-          <p>
-            CORS is a node. js package for providing a Connect/Express
-            middleware that can be used to enable CORS with various options.
-            This module supports validating the origin dynamically using a
-            function provided to the origin option. This function will be passed
-            a string that is the origin. Certain CORS requests are considered
-            ‘complex’ and require an initial OPTIONS request (called the
-            “pre-flight request”).
-          </p>
+  <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="mb-10 border-t border-b divide-y">
+        <div className="grid py-8 sm:grid-cols-4">
+          <div className="mb-4 sm:mb-0">
+            <div className="space-y-1 text-xs font-semibold tracking-wide uppercase">
+              <a
+                href="/"
+                className="transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                aria-label="Category"
+              >
+                CORS
+              </a>
+              <p className="text-gray-600">26 OCT 2022</p>
+            </div>
+          </div>
+          <div className="sm:col-span-3 lg:col-span-2">
+            <div className="mb-3">
+              <a
+                href="/"
+                aria-label="Article"
+                className="inline-block text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+              >
+                <p className="text-3xl font-extrabold leading-none sm:text-4xl xl:text-4xl">
+                  What is cors?
+                </p>
+              </a>
+            </div>
+            <p className="text-gray-700">
+              Cross-Origin Resource Sharing (CORS) is an HTTP-based security mechanism controlled and enforced by the client (web browser). It allows a service (API) to indicate any origin other than its own from which the client can request resources. It has been designed in response to the same-origin policy (SOP) that restricts how a website (HTML document or JS script) loaded by one origin can interact with a resource from another origin. CORS is used to explicitly allow some cross-origin requests while rejecting others.
+            </p>
+          </div>
+        </div>
+        <div className="grid py-8 sm:grid-cols-4">
+          <div className="mb-4 sm:mb-0">
+            <div className="space-y-1 text-xs font-semibold tracking-wide uppercase">
+              <a
+                href="/"
+                className="transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                aria-label="Category"
+              >
+                Firebase
+              </a>
+              <p className="text-gray-600">15 Sep 2022</p>
+            </div>
+          </div>
+          <div className="sm:col-span-3 lg:col-span-2">
+            <div className="mb-3">
+              <a
+                href="/"
+                aria-label="Article"
+                className="inline-block text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+              >
+                <p className="text-3xl font-extrabold leading-none sm:text-4xl xl:text-4xl">
+                  Why are you using firebase? 
+                </p>
+              </a>
+            </div>
+            <p className="text-gray-700">
+              Firebase helps you develop high-quality apps, grow your user base, and earn more money. Each feature works independently, and they work even better together.
+            </p>
+          </div>
+        </div>
+        <div className="grid py-8 sm:grid-cols-4">
+          <div className="mb-4 sm:mb-0">
+            <div className="space-y-1 text-xs font-semibold tracking-wide uppercase">
+              <a
+                href="/"
+                className="transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                aria-label="Category"
+              >
+                Implement authentication
+              </a>
+              <p className="text-gray-600">28 Dec 2022</p>
+            </div>
+          </div>
+          <div className="sm:col-span-3 lg:col-span-2">
+            <div className="mb-3">
+              <a
+                href="/"
+                aria-label="Article"
+                className="inline-block text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+              >
+                <p className="text-3xl font-extrabold leading-none sm:text-4xl xl:text-4xl">
+                  What other options do you have to implement authentication?
+                </p>
+              </a>
+            </div>
+            <p className="text-gray-700">
+              Firebase helps you develop high-quality apps, grow your user base, and earn more money. Each feature works independently, and they work even better together.Firebase completely freed our engineers from managing campaign infrastructure, and helped our team to be more effective and our campaigns to perform better—which is a huge win for us.
+            </p>
+          </div>
+        </div>
+        <div className="grid py-8 sm:grid-cols-4">
+          <div className="mb-4 sm:mb-0">
+            <div className="space-y-1 text-xs font-semibold tracking-wide uppercase">
+              <a
+                href="/"
+                className="transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                aria-label="Category"
+              >
+                Private Route
+              </a>
+              <p className="text-gray-600">28 OCT 2022</p>
+            </div>
+          </div>
+          <div className="sm:col-span-3 lg:col-span-2">
+            <div className="mb-3">
+              <a
+                href="/"
+                aria-label="Article"
+                className="inline-block text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+              >
+                <p className="text-3xl font-extrabold leading-none sm:text-4xl xl:text-4xl">
+                  How does the private route work?
+                </p>
+              </a>
+            </div>
+            <p className="text-gray-700">
+              The private route component is similar to the public route, the only change is that redirect URL and authenticate condition.
+If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).
+            </p>
+          </div>
+        </div>
+        <div className="grid py-8 sm:grid-cols-4">
+          <div className="mb-4 sm:mb-0">
+            <div className="space-y-1 text-xs font-semibold tracking-wide uppercase">
+              <a
+                href="/"
+                className="transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                aria-label="Category"
+              >
+                Node
+              </a>
+              <p className="text-gray-600">28 OCT 2022</p>
+            </div>
+          </div>
+          <div className="sm:col-span-3 lg:col-span-2">
+            <div className="mb-3">
+              <a
+                href="/"
+                aria-label="Article"
+                className="inline-block text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+              >
+                <p className="text-3xl font-extrabold leading-none sm:text-4xl xl:text-4xl">
+                  What is Node? How does Node work?
+                </p>
+              </a>
+            </div>
+            <p className="text-gray-700">
+              Node.js is a JavaScript runtime environment that achieves low latency and high throughput by taking a “non-blocking” approach to serving requests. In other words, Node.js wastes no time or resources on waiting for I/O requests to return.
+            </p>
+          </div>
         </div>
       </div>
-      <div className="mb-5 p-10 text-start border border-base-400 bg-base-200 rounded-box">
-        <div className="text-xl font-medium">
-          2. Why are you using firebase? What other options do you have to
-          implement authentication?
-        </div>
-        <div className="">
-          <p>
-            Firebase helps you develop high-quality apps, grow your user base,
-            and earn more money. Each feature works independently, and they work
-            even better together.Firebase completely freed our engineers from
-            managing campaign infrastructure, and helped our team to be more
-            effective and our campaigns to perform better—which is a huge win
-            for us.
-            <br />
-            Usually, authentication by a server entails the use of a user name
-            and password. a. Password-based authentication · b.
-            Two-factor/multifactor authentication · c. Biometric authentication
-            · d. Single sign-on · e. Token-based authentication.
-          </p>
-        </div>
-      </div>
-      <div className="mb-5 p-10 text-start border border-base-400 bg-base-200 rounded-box">
-        <div className="text-xl font-medium">
-          3. How does the private route work?
-        </div>
-        <div className="">
-          <p>
-            The private route component is similar to the public route, the only
-            change is that redirect URL and authenticate condition. If the user
-            is not authenticated he will be redirected to the login page and the
-            user can only access the authenticated routes If he is authenticated
-            (Logged in). Sets can take a private prop which makes all Routes
-            inside that Set require authentication.
-          </p>
-        </div>
-      </div>
-      <div className="mb-5 p-10 text-start border border-base-400 bg-base-200 rounded-box">
-        <div className="text-xl font-medium">
-          1. What is Node? How does Node work?
-        </div>
-        <div className="">
-          <p>
-            Node.js is written in C, C++, and JavaScript, and it is built on the
-            open-source V8 JavaScript engine which also powers JS in browsers
-            such as Google Chrome. As V8 supports new features in JavaScript,
-            they are incorporated into Node.
-            <br />
-            It is a used as backend service where javascript works on the
-            server-side of the application. This way javascript is used on both
-            frontend and backend.
-          </p>
-        </div>
+      <div className="text-center">
+        <a
+          href="/"
+          aria-label=""
+          className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+        >
+          See all blogs
+          <svg
+            className="inline-block w-3 ml-2"
+            fill="currentColor"
+            viewBox="0 0 12 12"
+          >
+            <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+          </svg>
+        </a>
       </div>
     </div>
   );
