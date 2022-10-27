8062fc41f576f2c2109c9badc95c8148217959b8
Jakaria230161 <jakariamahmud016@gmail.com>
2022-10-27 10:42:46 +0600

    home setup

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,11 +6,11 @@ import FAQ from "../FAQ/FAQ";
 export default function Home() {
   return (
     <>
-      <section className="bg-gray-800 text-gray-100">
+      <section className="bg-gray-900 text-white my-10 rounded-lg">
 	<div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
 		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-			<h1 className="text-5xl font-bold leading-none sm:text-6xl">Welcome to
-				<span className="dark:text-violet-400">WebConcepts</span>let's start...
+			<h1 className="text-4xl font-bold leading-none sm:text-6xl">Welcome to
+				<span className="dark:text-violet-400">WebConcepts</span>
 			</h1>
 			<p className="mt-6 mb-8 text-lg sm:mb-12">“Develop a passion for learning. If you do, you will never cease to grow.” 
               <br />
@@ -22,7 +22,7 @@ export default function Home() {
 			</div>
 		</div>
 		<div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-			<img src="https://img.freepik.com/free-photo/professional-programmer-working-late-dark-office_1098-18705.jpg?w=740&t=st=1666808675~exp=1666809275~hmac=04ec590e1e0f437d0eccc187ca4b572f7d1901ad6d3ba91667de25d0d766685e" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
+			<img src="https://img.freepik.com/free-photo/top-view-unrecognizable-hacker-performing-cyberattack-night_1098-18706.jpg?w=360&t=st=1666845352~exp=1666845952~hmac=6be8e633f252520de0ba431c27bb988aa3e1912062db62775e16eee06b31765d" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
 		</div>
 	</div>
 </section>
